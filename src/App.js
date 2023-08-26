432ac4e35a9fd6fefbf9e6c3821619b298ac68d6
subham13-02 <sahu21019@gmail.com>
2023-08-26 23:45:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar"
+import "./App.css"
+const App=()=>{
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import React,{useState} from "react"
+import "./NavBar.css"
+
+
+const NavBar=(bookData)=>{
+    const [search,setSearch]=useState("")
+
+    return (
+        <div className="navBar"> 
+            <div className="store-name">
+                <img src={require("./Images/brand-logo.png")}  alt="logo"/>
+                <h2>BooK StorE</h2>
+            </div>
+            <div className="search-bar">
+                <div className="search">
+                    <img src={require("./Images/search-icon.png")}  alt="logo"/>
+                    <input type="text" value={search} placeholder="Search for the Book you want study....." onChange={(e)=>{setSearch(e.value)}}/>
+                </div>
+                <button>Search</button>
+            </div>
+            <div className="icons">
+                <img src={require("./Images/icon1.png")}  alt="icon1"/>
+                <img src={require("./Images/icon2.png")} alt="icon2"/>
+                <img src={require("./Images/icon3.png")}  alt="icon3"/>
+                <img src={require("./Images/dp.png")}  alt="img"/>
+
+            </div>
+        </div>
+    )
+}
+export default NavBar;
