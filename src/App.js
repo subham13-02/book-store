aa10335e205c49cb7367054cfb6fc8406f06860f
subham13-02 <sahu21019@gmail.com>
2023-08-28 15:58:02 +0530

    fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from "react";
-import NavBar from "./Components/NavBar/NavBar"
+import React,{useState,useEffect} from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import MoreBooks from "./Components/MoreBooks/MoreBooks";
 import "./App.css"
-const App=()=>{
+import axios from "axios"
+
+const App =()=>{
+  const [booksDetails1,setBooksDetails1]=useState([]);
+  const [booksDetails2,setBooksDetails2]=useState([]);
+  const url1="https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes";
+  const url2="https://www.googleapis.com/books/v1/volumes?q=harry+potter";
+ 
+ 
+useEffect(()=>{
+      axios.get(url1)
+      .then((response)=>{
+          setBooksDetails1(response.data.items);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+
+      axios.get(url2)
+      .then((response)=>{
+          setBooksDetails2(response.data.items);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+  },[]);
+
+  
+
   return (
     <div className="App">
       <NavBar />
+      <MoreBooks booksDetails1={booksDetails1} booksDetails2={booksDetails2}/>
+      
     </div>
   );
 }
---
+++ src/Components/MoreBooks/MoreBooks.js
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from "react";
+
+const MoreBooks=({booksDetails1, booksDetails2})=>{
+    
+  const [bookImages,setBookImages]=useState([]);
+    
+  useEffect(() => {
+        const images1 = booksDetails1.map(book => book.volumeInfo.imageLinks.thumbnail);
+        const images2 = booksDetails2.map(book => book.volumeInfo.imageLinks.thumbnail);
+        setBookImages([...images1, ...images2]);
+    }, [booksDetails1, booksDetails2]);
+    // console.log(bookImages)
+    return(
+        <div className="moreBooks">
+        <h2>More Books</h2>
+        {
+            bookImages.map((imageLink, index) => (
+                <img key={index} src={imageLink} alt={`img${index}`} />
+            ))
+        }
+    </div>
+    )
+}
+export default MoreBooks;
